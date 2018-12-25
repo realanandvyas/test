51b1b7fb0fba6a89972c33797a34b60ce223606e
realanandvyas <anand.x.vyas@gmail.com>
2018-12-25 09:44:40 +0530

    Second Commit

--- NewModule/src/NewPackage/MyClass.java
+++ NewModule/src/NewPackage/MyClass.java
@@ -3,16 +3,25 @@ package NewPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 
 public class MyClass {
     public static void main(String[] args){
         ChromeDriver driver=new ChromeDriver();
-        driver.get("https://release.datakonnect.com");
-        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
-        element.sendKeys("abc@gmail.com");
 
-        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
-        button.click();
+
+        driver.get("https://release.datakonnect.com");
+
+        driver.manage().window().maximize();
+
+        // find the Click button
+        driver.findElement(By.id("startIco")).click();
+
+
     }
 }
